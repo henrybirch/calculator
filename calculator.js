3f4279287e2d0a9f9da0c7e984d4d33dee9cedef
henrybirch <hab.b@hotmail.co.uk>
2023-02-21 23:45:54 +0000

    Add js so pages can work

---
+++ calculator.js
@@ -0,0 +1,122 @@
+function number(n) {
+    return { exp: function () { return n; }, toString: n.toString() };
+}
+function plus(a, b) {
+    return { exp: function () { return a.exp() + b.exp(); }, toString: "(".concat(a.toString, " + ").concat(b.toString, ")") };
+}
+function minus(a, b) {
+    return { exp: function () { return a.exp() - b.exp(); }, toString: "(".concat(a.toString, " + ").concat(b.toString, ")") };
+}
+function multiply(a, b) {
+    return { exp: function () { return a.exp() * b.exp(); }, toString: "(".concat(a.toString, " * ").concat(b.toString, ")") };
+}
+function divide(a, b) {
+    return { exp: function () { return a.exp() / b.exp(); }, toString: "(".concat(a.toString, " / ").concat(b.toString, ")") };
+}
+function addNumberButtons() {
+    var numbers = document.getElementById("numbers");
+    var _loop_1 = function (i) {
+        var button = document.createElement("div");
+        button.id = i.toString();
+        button.textContent = i.toString();
+        button.classList.add("number");
+        button.classList.add("keypad-button");
+        button.addEventListener("click", function (e) { return doNumber(number(i)); });
+        numbers.appendChild(button);
+    };
+    for (var i = 0; i < 10; i++) {
+        _loop_1(i);
+    }
+}
+function addOperationButtons() {
+    var operations = document.getElementById("operations");
+    function makeOperationElement(id, listener) {
+        var operation = document.createElement("div");
+        operation.id = id;
+        operation.textContent = id;
+        operation.classList.add("operation");
+        operation.classList.add("keypad-button");
+        operation.addEventListener("click", function (e) { return listener(); });
+        return operation;
+    }
+    operations.appendChild(makeOperationElement("+", doPlus));
+    operations.appendChild(makeOperationElement("-", doMinus));
+    operations.appendChild(makeOperationElement("*", doMultiply));
+    operations.appendChild(makeOperationElement("/", doDivide));
+}
+function doNumber(n) {
+    function setVars() {
+        if (!isFirstNumber || !isOp) {
+            firstNumber = n;
+            isFirstNumber = true;
+            return;
+        }
+        if (isOp) {
+            secondNumber = n;
+            isSecondNumber = true;
+            return;
+        }
+    }
+    setVars();
+    setDisplay(n.exp().toString());
+}
+function canSetOperator() {
+    return isFirstNumber && !isSecondNumber;
+}
+function setDisplay(s) {
+    var display = document.getElementById("screen");
+    display.textContent = s;
+}
+function doOp(opString, func) {
+    op = function (x) { return func(firstNumber, x); };
+    isOp = true;
+    setDisplay(opString);
+}
+function doPlus() {
+    if (canSetOperator()) {
+        doOp("+", plus);
+    }
+}
+function doMinus() {
+    if (canSetOperator()) {
+        doOp("-", minus);
+    }
+}
+function doMultiply() {
+    if (canSetOperator()) {
+        doOp("*", multiply);
+    }
+}
+function doDivide() {
+    if (canSetOperator()) {
+        doOp("/", divide);
+    }
+}
+function doClear() {
+    curr = null;
+    isOp = false;
+    isFirstNumber = false;
+    isSecondNumber = false;
+    firstNumber = null;
+    secondNumber = null;
+    op = null;
+    setDisplay("");
+}
+function doCalc() {
+    if (!isOp || !isFirstNumber || !isSecondNumber)
+        return;
+    curr = op(secondNumber);
+    setDisplay(curr.toString + " = " + curr.exp().toString());
+    firstNumber = curr;
+    isSecondNumber = false;
+    isOp = false;
+}
+addNumberButtons();
+addOperationButtons();
+var curr = null;
+var isOp = false;
+var isFirstNumber = false;
+var isSecondNumber = false;
+var firstNumber = null;
+var secondNumber = null;
+var op = null;
